aa2ccd25db9d651d967932ce6f65ce7f63b4a3d4
dev.ksanbal <dev.ksanbal@gmail.com>
2021-11-11 23:01:57 +0900

    Fix. DB Model 변수명 수정

--- api/src/models.py
+++ api/src/models.py
@@ -3,82 +3,84 @@ from src.database import Base
 from sqlalchemy import Column, Integer, String, Float
 
 class Area(Base):
-    __tablename__ = "area"
+    __tablename__ = "AREA_TB"
     
-    area_code = Column(Integer, primary_key=True)
-    area_name = Column(String(20))
-    latitude = Column(Float)
-    longitude = Column(Float)
-    r_value = Column(Integer)
+    areaCode = Column("area_code", Integer, primary_key=True)
+    areaName = Column("area_name", String(20))
+    latitude = Column("latitude", Float)
+    longitude = Column("longitude", Float)
+    rValue = Column("r_value", Integer)
     
 class Businesss(Base):
-    __tablename__ = "business"
+    __tablename__ = "BUSINESS_TB"
     
-    business_code = Column(Integer, primary_key=True)
-    business_name = Column(String(20))
+    businessCode = Column("business_code", Integer, primary_key=True)
+    businessName = Column("business_name", String(20))
     
 class Store(Base):
-    __tablename__ = "store"
+    __tablename__ = "STORE_TB"
     
-    id = Column(Integer, primary_key=True, index=True)
-    sotre_name = Column(String(20))
-    area_code = Column(Integer, ForeignKey('area.area_code'))
-    business_code = Column(Integer, ForeignKey('business.business_code'))
-    latitude = Column(Float)
-    longitude = Column(Float)
+    id = Column("id", Integer, primary_key=True, index=True)
+    sotreName = Column("sotre_name", String(20))
+    areaCode = Column("area_code_fk", Integer, ForeignKey('AREA_TB.area_code'))
+    businessCode = Column("business_code_fk", Integer, ForeignKey('BUSINESS_TB.business_code'))
+    latitude = Column("latitude", Float)
+    longitude = Column("longitude", Float)
     
 class Change(Base):
-    __tablename__ = "change"
+    __tablename__ = "CHANGE_TB"
     
-    id = Column(Integer, primary_key=True, index=True)
-    area_code = Column(Integer, ForeignKey('area.area_code'))
-    business_code = Column(Integer, ForeignKey('business.business_code'))
-    status = Column(String(10))
-    closure = Column(Float)
+    id = Column("id", Integer, primary_key=True, index=True)
+    areaCode = Column("area_code_fk", Integer, ForeignKey('AREA_TB.area_code'))
+    businessCode = Column("business_code_fk", Integer, ForeignKey('BUSINESS_TB.business_code'))
+    status = Column("status", String(10))
+    closure = Column("closure", Float)
     
 class Sales(Base):
-    __tablename__ = "sales"
+    __tablename__ = "SALES_TB"
     
-    id = Column(Integer, primary_key=True, index=True)
-    area_code = Column(Integer, ForeignKey('area.area_code'))
-    business_code = Column(Integer, ForeignKey('business.business_code'))
-    amount = Column(Integer)
+    id = Column("id", Integer, primary_key=True, index=True)
+    areaCode = Column("area_code_fk", Integer, ForeignKey('AREA_TB.area_code'))
+    businessCode = Column("business_code_fk", Integer, ForeignKey('BUSINESS_TB.business_code'))
+    amount = Column("amount", Integer)
     
 class DaySales(Base):
-    __tablename__ = "daySales"
+    __tablename__ = "DAYSALES_TB"
     
-    id = Column(Integer, primary_key=True, index=True)
-    sales_id = Column(Integer, ForeignKey('sales.id'))
-    monday_ratio = Column(Integer)
-    tuesday_ratio = Column(Integer)
-    wednesday_ratio = Column(Integer)
-    thursday_ratio = Column(Integer)
-    friday_ratio = Column(Integer)
-    saturday_ratio = Column(Integer)
-    sunday_ratio = Column(Integer)
+    id = Column("id", Integer, primary_key=True, index=True)
+    salesId = Column("sales_id_fk", Integer, ForeignKey('SALES_TB.id'))
+    mondayRatio = Column("monday_ratio", Integer)
+    tuesdayRatio = Column("tuesday_ratio", Integer)
+    wednesdayRatio = Column("wednesday_ratio", Integer)
+    thursdayRatio = Column("thursday_ratio", Integer)
+    fridayRatio = Column("friday_ratio", Integer)
+    saturdayRatio = Column("saturday_ratio", Integer)
+    sundayRatio = Column("sunday_ratio", Integer)
     
 class TimeSales(Base):
-    __tablename__ = "timeSales"
+    # __tablename__ = "timeSales"
+    __tablename__ = "TIMESALES_TB"
     
-    id = Column(Integer, primary_key=True, index=True)
-    sales_id = Column(Integer, ForeignKey('sales.id'))
-    time_0006 = Column(Integer)
-    time_0611 = Column(Integer)
-    time_1114 = Column(Integer)
-    time_1417 = Column(Integer)
-    time_1721 = Column(Integer)
-    time_2124 = Column(Integer)
+    id = Column("id", Integer, primary_key=True, index=True)
+    salesId = Column("sales_id_fk", Integer, ForeignKey('SALES_TB.id'))
+    time0006 = Column("time_0006", Integer)
+    time0611 = Column("time_0611", Integer)
+    time1114 = Column("time_1114", Integer)
+    time1417 = Column("time_1417", Integer)
+    time1721 = Column("time_1721", Integer)
+    time2124 = Column("time_2124", Integer)
     
 class CustomerSales(Base):
-    __tablename__ = "custommerSales"
+    # __tablename__ = "custommerSales"
+    __tablename__ = "CUSTOMMERSALES_TB"
     
-    id = Column(Integer, primary_key=True, index=True)
-    sales_id = Column(Integer, ForeignKey('sales.id'))
-    man = Column(Integer)
-    woman = Column(Integer)
-    age_10 = Column(Integer)
-    age_20 = Column(Integer)
-    age_30 = Column(Integer)
-    age_40 = Column(Integer)
-    age_50 = Column(Integer)
-    age_60 = Column(Integer)
+    id = Column("id", Integer, primary_key=True, index=True)
+    salesId = Column("sales_id_fk", Integer, ForeignKey('SALES_TB.id'))
+    man = Column("man", Integer)
+    woman = Column("woman", Integer)
+    age10 = Column("age_10", Integer)
+    age20 = Column("age_20", Integer)
+    age30 = Column("age_30", Integer)
+    age40 = Column("age_40", Integer)
+    age50 = Column("age_50", Integer)
+    age60 = Column("age_60", Integer)
